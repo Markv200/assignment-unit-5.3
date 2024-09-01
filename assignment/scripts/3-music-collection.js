7b64b5d05844cfddf7a0fb215c445f2c7f7c6907
Mark Velesaca <mark.velesaca@gmail.com>
2024-09-01 01:49:56 -0500

    songs

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,37 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 let myCollection = [];
+console.log(myCollection);
+function addToCollection(collection, title, artist, yearPublished){
+  Album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished 
+  };
+  collection.push(Album);
 
+  return Album;
 
+}
 
+console.log(addToCollection(myCollection,'When I was Your Man','Bruno Mars', 2012));
+console.log(addToCollection(myCollection,'Gata Only','Floyymenor', 2024));
+console.log(addToCollection(myCollection,'Heartless','Kanye West', 2008));
+console.log(addToCollection(myCollection,'You Belong With Me' , 'Taylor Swift' , 2021));
+console.log(addToCollection(myCollection,'Trance','Metro Boomin & Travis Scott', 2022));
+console.log(addToCollection(myCollection,'Sad Girl', 'Lana Del Ray', 2013));
+console.log(myCollection);
+
+function showCollection(collection){
+  for(let Album of collection){
+  console.log(`${Album.title} by ${Album.artist}, was published in ${Album.yearPublished}`);
+  }
+}
+showCollection(myCollection);
+
+function findByArtist(collection, artist){
+
+}
 
 
 
